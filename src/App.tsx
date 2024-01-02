cac57177f8ca04f05d47f8c0145388965d5955be
Minseo Kang <rachel5640@naver.com>
2024-01-03 07:35:09 +0900

    feat:app.tsx에 추가 (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import GlobalStyle from "./styles/GlobalStyle";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <GlobalStyle />
+    </div>
+  );
 };
 
 export default App;
