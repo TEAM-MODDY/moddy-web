14cccd701e3521b8ce849491ee7cb7192ccf3cc8
aazkgh <gg25hp17@naver.com>
2024-01-06 23:18:43 +0900

    design: 지원서 최종 확인 화면 퍼블리싱

--- src/pages/ApplicationPage.tsx
+++ src/pages/ApplicationPage.tsx
@@ -1,5 +1,23 @@
+import { styled } from 'styled-components';
+
+import ApplicationInfo from '../views/ApplicationPage/components/ApplicationInfo';
+
 const ApplicationPage = () => {
-  return <div></div>;
+  return (
+    <ApplicationPagSection>
+      <ApplicationInfo />
+    </ApplicationPagSection>
+  );
 };
 
 export default ApplicationPage;
+
+const ApplicationPagSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  width: 100%;
+  height: 100vh;
+  padding: 0 2rem;
+`;
