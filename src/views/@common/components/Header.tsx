329c1d217cd2103174c6c3afca934951ef72215c
Minseo Kang <111034927+rachel5640@users.noreply.github.com>
2024-01-07 04:52:24 +0900

    [ModelInfoPage]첫번째 영역 퍼블리싱 (#35)
    * init: 초기 페이지 설정
    
    * design: 전체 화면 구조 퍼블리싱
    
    * feat: Records 동적 생성
    
    * feat: 컴포넌트, 타입 분리
    
    * desgin: 글 줄바꿈 처리
    
    * [Common] 공통 컴포넌트 - Header, CTA 구현 (#33)
    
    * init: ThemeProvide 위치 이동
    
    * design: Common Button Publishing
    
    * design: Common Header Publishing
    
    * design: 레이아웃 수정
    
    * design: App.tsx 모바일 리사이즈 코드 추가
    
    * design: Header props를 통한 조건부 publishing
    
    * design: Button props를 통한 조건부 publishing
    
    * feat: Header에 onClick함수 넣을 수 있도록 props 추가
    
    * chore: MainPage 잘못 푸시한 내용 원상복귀
    
    * chore: 불필요한 className 삭제
    
    * desgin: Header, Cta 추가
    
    * feat: cta 클릭시 Navigate
    
    * fix: Route 경로 수정
    
    ---------
    
    Co-authored-by: lydiacho <81505421+lydiacho@users.noreply.github.com>

---
+++ src/views/@common/components/Header.tsx
@@ -0,0 +1,51 @@
+import { styled } from 'styled-components';
+
+import { IcCloseBlack, IcLeftBlack } from '../assets/icons';
+
+interface HeaderProps {
+  isBackBtnExist?: boolean;
+  isCloseBtnExist?: boolean;
+  title: string;
+  backFn?: () => void;
+  closeFn?: () => void;
+}
+
+const Header = ({ isBackBtnExist, isCloseBtnExist, title, backFn, closeFn }: HeaderProps) => {
+  return (
+    <S.HeaderLayout>
+      <S.HeaderBox>
+        <button onClick={backFn}>{isBackBtnExist ? <IcLeftBlack /> : <S.HeaderBlankBox />}</button>
+        <S.HeaderH1>{title}</S.HeaderH1>
+        <button onClick={closeFn}>{isCloseBtnExist ? <IcCloseBlack /> : <S.HeaderBlankBox />}</button>
+      </S.HeaderBox>
+    </S.HeaderLayout>
+  );
+};
+
+export default Header;
+
+const S = {
+  HeaderLayout: styled.section`
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 1;
+
+    width: 100%;
+    padding: 0.9rem 1.6rem 1.7rem 1.5rem;
+
+    background-color: ${({ theme }) => theme.colors.moddy_wt};
+  `,
+  HeaderBox: styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `,
+  HeaderH1: styled.h1`
+    ${({ theme }) => theme.fonts.Headline01};
+  `,
+  HeaderBlankBox: styled.div`
+    width: 2.4rem;
+    height: 2.4rem;
+  `,
+};
