987daa97cd7654e735654cfda7ca10402094d585
binllionaire <bug20474@gmail.com>
2024-01-07 04:09:14 +0900

    design : 메인 페이지 퍼블리싱

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,19 @@
+import Banner from '../views/MainPage/components/Banner';
+import Contents from '../views/MainPage/components/Contents';
+import ReceivedOffer from '../views/MainPage/components/ReceivedOffer';
+import TopSheet from '../views/MainPage/components/TopSheet';
+import { APPLY_TYPE, USER_TYPE } from '../views/MainPage/constants/constants';
+
 const MainPage = () => {
-  return <div></div>;
+  const userType = USER_TYPE.GUEST;
+  const applyType = APPLY_TYPE.NOT_YET;
+  return (
+    <>
+      <TopSheet userType={userType} applyType={applyType} />
+      <Banner />
+      {userType === USER_TYPE.GUEST ? <Contents /> : <ReceivedOffer applyType={applyType} />}
+    </>
+  );
 };
 
 export default MainPage;
