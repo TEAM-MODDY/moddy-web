086ca02a09f0f15c074392e0804ebd1c3a1ebf28
lydiacho <sheecho0804@ewhain.net>
2024-01-06 16:55:12 +0900

    chore: assets images 저장 및 추출

---
+++ src/views/@common/assets/images/index.ts
@@ -0,0 +1,8 @@
+import ImgKakaotalk from './img_kakaotalk.svg';
+import ImgLogoBlack from './img_logo_black.svg';
+import ImgLogoBlue from './img_logo_blue.svg';
+import ImgLogoWhite from './img_logo_white.svg';
+import ImgWaxsealsBlue from './img_waxseals_blue.svg';
+import ImgWaxsealsWhite from './img_waxseals_white.svg';
+
+export { ImgKakaotalk, ImgLogoBlack, ImgLogoBlue, ImgLogoWhite, ImgWaxsealsBlue, ImgWaxsealsWhite };
