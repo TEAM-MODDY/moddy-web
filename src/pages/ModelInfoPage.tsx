ac11ea627ea8bc678bb8303d091c18a2ffb48908
Minseo Kang <rachel5640@naver.com>
2024-01-06 20:18:56 +0900

    init: 초기 페이지 설정

--- src/pages/ModelInfoPage.tsx
+++ src/pages/ModelInfoPage.tsx
@@ -1,5 +1,22 @@
+/* eslint-disable import/no-named-as-default */
+import styled from 'styled-components';
+
 const ModelInfoPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <S.ImageBox></S.ImageBox>
+    </div>
+  );
+};
+
+const S = {
+  ImageBox: styled.div`
+    width: 34.4rem;
+    height: 34.4rem;
+    border-radius: 10px;
+
+    background-color: ${({ theme }) => theme.colors.kakao};
+  `,
 };
 
 export default ModelInfoPage;
