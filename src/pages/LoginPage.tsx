70c0a741c079343c638fe4e2dfcc7fee3b059814
lydiacho <sheecho0804@ewhain.net>
2024-01-07 22:20:09 +0900

    feat: 뒤로가기 버튼 navigate 추가

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,12 +1,20 @@
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 import Header from '../views/@common/components/Header';
 import LoginText from '../views/LoginPage/components/LoginText';
 
 const LoginPage = () => {
+  const navigate = useNavigate();
   return (
     <S.LoginPageLayout>
-      <Header title="" isBackBtnExist />
+      <Header
+        title=""
+        isBackBtnExist
+        backFn={() => {
+          navigate(-1);
+        }}
+      />
       <LoginText />
     </S.LoginPageLayout>
   );
