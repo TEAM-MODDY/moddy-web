516d898f244f144a93b78b151957daa670f7c86a
lydiacho <sheecho0804@ewhain.net>
2024-01-08 02:02:52 +0900

    feat: 이용약관 체크 여부에 따른 버튼 비활성화

--- src/pages/AgreementPage.tsx
+++ src/pages/AgreementPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import Button from '../views/@common/components/Button';
@@ -5,12 +6,18 @@ import Header from '../views/@common/components/Header';
 import AgreementList from '../views/AgreementPage/components/AgreementList';
 
 const AgreementPage = () => {
+  const [isChecked, setChecked] = useState<boolean[]>(new Array(4).fill(false));
   const navigate = useNavigate();
   return (
     <div>
       <Header title="이용약관" isBackBtnExist backFn={() => navigate(-1)} />
-      <AgreementList />
-      <Button text="다음" isFixed onClickFn={() => navigate('/sign-up')} />
+      <AgreementList isChecked={isChecked} setChecked={setChecked} />
+      <Button
+        text="다음"
+        isFixed
+        onClickFn={() => navigate('/sign-up')}
+        disabled={!isChecked[1] || !isChecked[2] || !isChecked[3]}
+      />
     </div>
   );
 };
