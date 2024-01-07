de6121983b3501625065f8f78b7eea6b04e82d8d
lydiacho <sheecho0804@ewhain.net>
2024-01-07 17:55:09 +0900

    design: Header, CTA 추가

--- src/pages/AgreementPage.tsx
+++ src/pages/AgreementPage.tsx
@@ -1,5 +1,16 @@
+import { useNavigate } from 'react-router-dom';
+
+import Button from '../views/@common/components/Button';
+import Header from '../views/@common/components/Header';
+
 const AgreementPage = () => {
-  return <div>AgreementPage</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <Header title="이용약관" isBackBtnExist backFn={() => navigate(-1)} />
+      <Button text="다음" isFixed onClickFn={() => console.log('임시 클릭함수')} />
+    </div>
+  );
 };
 
 export default AgreementPage;
