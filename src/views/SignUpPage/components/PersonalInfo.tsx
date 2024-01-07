18a2ed263d925605d3b1751e475cec2f28cecda1
binllionaire <bug20474@gmail.com>
2024-01-07 16:11:04 +0900

    design : 개인 정보 입력 페이지 퍼블리싱

---
+++ src/views/SignUpPage/components/PersonalInfo.tsx
@@ -0,0 +1,9 @@
+interface PersonalInfoProp {
+  setStep: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const PersonalInfo = ({ setStep }: PersonalInfoProp) => {
+  return <></>;
+};
+
+export default PersonalInfo;
