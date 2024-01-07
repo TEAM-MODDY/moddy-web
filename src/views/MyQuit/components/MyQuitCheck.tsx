488cd39e7e1eacde52640fe354cffff5fb03b358
lydiacho <sheecho0804@ewhain.net>
2024-01-07 23:51:12 +0900

    feat: MyQuitCheck 구현

--- src/views/MyQuit/components/MyQuitCheck.tsx
+++ src/views/MyQuit/components/MyQuitCheck.tsx
@@ -1,7 +1,35 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
+import { IcCheckboxBlue, IcCheckboxGrey } from '../../@common/assets/icons';
+
 const MyQuitCheck = () => {
-  return <div>MyQuitCheck</div>;
+  const [isChecked, setChecked] = useState(false);
+  return (
+    <S.MyQuitCheckLayout type="button" onClick={() => setChecked(!isChecked)}>
+      {isChecked ? <IcCheckboxBlue /> : <IcCheckboxGrey />}
+      <S.MyQuitCheckParagraph>위 내용을 모두 숙지했으며 탈퇴에 동의합니다.</S.MyQuitCheckParagraph>
+    </S.MyQuitCheckLayout>
+  );
 };
 
 export default MyQuitCheck;
+
+const S = {
+  MyQuitCheckLayout: styled.button`
+    display: flex;
+    gap: 1.1rem;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    bottom: 0;
+
+    width: 100%;
+    max-width: 43rem;
+    margin-bottom: 11.5rem;
+  `,
+  MyQuitCheckParagraph: styled.p`
+    color: ${({ theme }) => theme.colors.moddy_bk};
+    ${({ theme }) => theme.fonts.Body01};
+  `,
+};
