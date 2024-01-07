602eba2559a9f93596d3532173a79a9ccfbb3fde
Minseo Kang <rachel5640@naver.com>
2024-01-07 22:06:58 +0900

    design: 제목 컴포넌트 제작

--- src/views/@common/components/ModelOffer.tsx
+++ src/views/@common/components/ModelOffer.tsx
@@ -1,11 +1,30 @@
-import Header from './Header';
+// import Header from './Header';
+import { styled } from 'styled-components';
+
+import TitleBox from '../../ModelInfoPage/components/TitleBox';
+
 const ModelOffer = () => {
   return (
-    <div>
-      <Header isBackBtnExist={true} isCloseBtnExist={true} title="헤어 모델 제안하기" />
-      오퍼페이지
-    </div>
+    <S.ModelOfferLayout>
+      <S.ModelOfferBox>
+        <TitleBox title="희망 제안 조건" subtitle="원하시는 조건을 모두 선택해주세요" isNeccessary={true} />
+      </S.ModelOfferBox>
+      <S.ModelOfferBox>
+        <TitleBox title="상세 제안" subtitle="자세히 적을 수록 매칭 성공률이 높아져요" isNeccessary={true} />
+      </S.ModelOfferBox>
+    </S.ModelOfferLayout>
   );
+};
+
+const S = {
+  ModelOfferLayout: styled.section`
+    margin-top: 5.7rem;
+    padding: 0 1.6rem;
+  `,
+
+  ModelOfferBox: styled.div`
+    margin-bottom: 3.85rem;
+  `,
 };
 
 export default ModelOffer;
