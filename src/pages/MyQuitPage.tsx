06c7b94a36074e0649d21ffcd919c439b1150173
lydiacho <sheecho0804@ewhain.net>
2024-01-07 23:45:31 +0900

    design: MyQuitPage 구성

--- src/pages/MyQuitPage.tsx
+++ src/pages/MyQuitPage.tsx
@@ -1,7 +1,28 @@
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
+import Button from '../views/@common/components/Button';
+import Header from '../views/@common/components/Header';
+import MyQuitCheck from '../views/MyQuit/components/MyQuitCheck';
+import MyQuitText from '../views/MyQuit/components/MyQuitText';
+
 const MyQuitPage = () => {
-  return <div>MyQuitPage</div>;
+  const navigate = useNavigate();
+  return (
+    <S.MyQuitPageLayout>
+      <Header title="회원탈퇴" isBackBtnExist backFn={() => navigate(-1)} />
+      <MyQuitText />
+      <MyQuitCheck />
+      <Button text="확인하기" onClickFn={() => console.log('test')} isFixed />
+    </S.MyQuitPageLayout>
+  );
 };
 
 export default MyQuitPage;
+
+const S = {
+  MyQuitPageLayout: styled.div`
+    display: flex;
+    flex-direction: column;
+  `,
+};
