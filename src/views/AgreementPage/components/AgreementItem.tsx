ecf8086a713593d93c31a1763afcecc5ba1de962
lydiacho <sheecho0804@ewhain.net>
2024-01-07 18:24:06 +0900

    feat: 이용약관 항목 체크박스 구현

--- src/views/AgreementPage/components/AgreementItem.tsx
+++ src/views/AgreementPage/components/AgreementItem.tsx
@@ -1,13 +1,37 @@
 import styled from 'styled-components';
 
-const AgreementItem = () => {
-  return <S.AgreementItemLayout>AgreementItem</S.AgreementItemLayout>;
+import { IcCheckboxBlue, IcCheckboxGrey } from '../../@common/assets/icons';
+
+interface AgreementItemProps {
+  firstItem?: boolean;
+  text: string;
+  isChecked: boolean;
+  setChecked: React.Dispatch<React.SetStateAction<boolean>>;
+}
+const AgreementItem = ({ firstItem, text, isChecked, setChecked }: AgreementItemProps) => {
+  return (
+    <S.AgreementItemLayout onClick={() => setChecked(!isChecked)}>
+      {isChecked ? <IcCheckboxBlue /> : <IcCheckboxGrey />}
+      <S.AgreementParagraph $firstItem={firstItem} $isChecked={isChecked}>
+        {text}
+      </S.AgreementParagraph>
+    </S.AgreementItemLayout>
+  );
 };
 
 export default AgreementItem;
 
 const S = {
   AgreementItemLayout: styled.div`
+    display: flex;
+    gap: 1.1rem;
+    align-items: center;
+
     width: 100%;
+  `,
+  AgreementParagraph: styled.p<{ $firstItem: boolean | undefined; $isChecked: boolean }>`
+    color: ${({ theme, $firstItem, $isChecked }) =>
+      $isChecked ? theme.colors.moddy_bk : $firstItem ? theme.colors.moddy_bk : theme.colors.moddy_gray50};
+    ${({ theme, $firstItem }) => ($firstItem ? theme.fonts.Body01 : theme.fonts.Body02)};
   `,
 };
--- src/views/AgreementPage/components/AgreementList.tsx
+++ src/views/AgreementPage/components/AgreementList.tsx
@@ -1,15 +1,22 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 import AgreementItem from './AgreementItem';
 
 const AgreementList = () => {
+  const [isCheckedAll, setCheckedAll] = useState(false);
+
+  const [isChecked1, setChecked1] = useState(false);
+  const [isChecked2, setChecked2] = useState(false);
+  const [isChecked3, setChecked3] = useState(false);
+
   return (
     <S.AgreementListLayout>
-      <AgreementItem />
+      <AgreementItem firstItem text="모든 약관에 동의합니다" isChecked={isCheckedAll} setChecked={setCheckedAll} />
       <S.AgreementLine />
-      <AgreementItem />
-      <AgreementItem />
-      <AgreementItem />
+      <AgreementItem text="모디 서비스 이용약관 동의" isChecked={isChecked1} setChecked={setChecked1} />
+      <AgreementItem text="개인정보 수집 및 이용 동의" isChecked={isChecked2} setChecked={setChecked2} />
+      <AgreementItem text="마케팅 정보 수신 동의 (선택)" isChecked={isChecked3} setChecked={setChecked3} />
     </S.AgreementListLayout>
   );
 };
