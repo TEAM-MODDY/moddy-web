a08e9dc563d0323728f3a32f986245709efe7e2e
binllionaire <bug20474@gmail.com>
2024-01-08 04:21:33 +0900

    feature : 조건부 로직 추가

--- src/views/SignUpPage/components/EnterProfile.tsx
+++ src/views/SignUpPage/components/EnterProfile.tsx
@@ -4,6 +4,8 @@ import Header from '../../@common/components/Header';
 import { STEP } from '../constants/step';
 
 import PersonalInfo from './PersonalInfo';
+import SelectPrefeRegion from './SelectPreferRegion';
+import VerifyPhoneNumber from './VerifyPhoneNumber';
 
 const EnterProfile = () => {
   const [step, setStep] = useState(STEP.PERSONAL_INFO);
@@ -12,8 +14,13 @@ const EnterProfile = () => {
     switch (step) {
       case STEP.PERSONAL_INFO:
         return <PersonalInfo setStep={setStep} />;
+      case STEP.PHONE_NUMBER_VERIFICATION:
+        return <VerifyPhoneNumber setStep={setStep} />;
+      case STEP.PREFER_REGION:
+        return <SelectPrefeRegion />;
     }
   };
+
   const StepHeader = () => {
     switch (step) {
       case STEP.PERSONAL_INFO:
