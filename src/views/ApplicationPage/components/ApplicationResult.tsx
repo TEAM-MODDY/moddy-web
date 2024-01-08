d750b1fcef6507b5f25ef7428c2c691d508eb8b0
aazkgh <gg25hp17@naver.com>
2024-01-08 15:56:28 +0900

    fix: textarea input을 paragraph 태그로 수정

--- src/views/ApplicationPage/components/ApplicationResult.tsx
+++ src/views/ApplicationPage/components/ApplicationResult.tsx
@@ -10,72 +10,76 @@ const ApplicationResult = () => {
   return (
     <S.ApplicationResultLayout>
       <Header isBackBtnExist={true} isCloseBtnExist={true} title="최종 확인"></Header>
-      <S.ContentBox>
-        <h2>
-          모델 정보 <ImgLogoBlue />
-        </h2>
+      <S.ContentSection>
+        <S.ContentBox>
+          <h2>
+            모델 정보 <ImgLogoBlue />
+          </h2>
+          <S.DivideBox>
+            <img alt="profile" src="src/views/@common/assets/images/img_samplemodel.png" />
+            <S.Info>
+              <li>
+                <S.InfoTitle>이름</S.InfoTitle>
+                <S.InfoSpan>백모디</S.InfoSpan>
+              </li>
+              <li>
+                <S.InfoTitle>성별/나이</S.InfoTitle>
+                <S.InfoSpan>여성/25살</S.InfoSpan>
+              </li>
+              <li>
+                <S.InfoTitle>희망 지역</S.InfoTitle>
+                <S.InfoSpan>양천구</S.InfoSpan>
+              </li>
+              <li>
+                <S.InfoTitle>현재 기장</S.InfoTitle>
+                <S.InfoSpan>허리 아래</S.InfoSpan>
+              </li>
+            </S.Info>
+          </S.DivideBox>
+        </S.ContentBox>
         <S.DivideBox>
-          <img alt="profile" src="src/views/@common/assets/images/img_samplemodel.png" />
-          <S.Info>
-            <li>
-              <S.InfoTitle>이름</S.InfoTitle>
-              <S.InfoSpan>백모디</S.InfoSpan>
-            </li>
-            <li>
-              <S.InfoTitle>성별/나이</S.InfoTitle>
-              <S.InfoSpan>여성/25살</S.InfoSpan>
-            </li>
-            <li>
-              <S.InfoTitle>희망 지역</S.InfoTitle>
-              <S.InfoSpan>양천구</S.InfoSpan>
-            </li>
-            <li>
-              <S.InfoTitle>현재 기장</S.InfoTitle>
-              <S.InfoSpan>허리 아래</S.InfoSpan>
-            </li>
-          </S.Info>
+          <S.ContentBox>
+            <h2>최근 시술 이력</h2>
+            <S.Info>
+              <li>
+                <S.InfoTitle>1개월 미만</S.InfoTitle>
+                <S.InfoSpan>블랙 염색</S.InfoSpan>
+              </li>
+              <li>
+                <S.InfoTitle>7 - 12개월</S.InfoTitle>
+                <S.InfoSpan>컬러 염색</S.InfoSpan>
+              </li>
+              <li>
+                <S.InfoTitle>12개월 초과</S.InfoTitle>
+                <S.InfoSpan>탈색</S.InfoSpan>
+              </li>
+            </S.Info>
+          </S.ContentBox>
+          <S.ContentBox>
+            <h2>희망 스타일</h2>
+            <S.Info>
+              <li>
+                <S.InfoTitle>커트</S.InfoTitle>
+                <S.InfoSpan>일반 커트</S.InfoSpan>
+              </li>
+              <li>
+                <S.InfoTitle>컬러</S.InfoTitle>
+                <S.InfoSpan>선택 없음</S.InfoSpan>
+              </li>
+              <li>
+                <S.InfoTitle>펌</S.InfoTitle>
+                <S.InfoSpan>일반펌</S.InfoSpan>
+              </li>
+            </S.Info>
+          </S.ContentBox>
         </S.DivideBox>
-      </S.ContentBox>
-      <S.DivideBox>
         <S.ContentBox>
-          <h2>최근 시술 이력</h2>
-          <S.Info>
-            <li>
-              <S.InfoTitle>1개월 미만</S.InfoTitle>
-              <S.InfoSpan>블랙 염색</S.InfoSpan>
-            </li>
-            <li>
-              <S.InfoTitle>7 - 12개월</S.InfoTitle>
-              <S.InfoSpan>컬러 염색</S.InfoSpan>
-            </li>
-            <li>
-              <S.InfoTitle>12개월 초과</S.InfoTitle>
-              <S.InfoSpan>탈색</S.InfoSpan>
-            </li>
-          </S.Info>
+          <h2>상세 희망 스타일</h2>
+          <S.InfoText>
+            가나다라마가나다라마가나다라마가나다라마가나다라마가가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마가나다라마
+          </S.InfoText>
         </S.ContentBox>
-        <S.ContentBox>
-          <h2>희망 스타일</h2>
-          <S.Info>
-            <li>
-              <S.InfoTitle>커트</S.InfoTitle>
-              <S.InfoSpan>일반 커트</S.InfoSpan>
-            </li>
-            <li>
-              <S.InfoTitle>컬러</S.InfoTitle>
-              <S.InfoSpan>선택 없음</S.InfoSpan>
-            </li>
-            <li>
-              <S.InfoTitle>펌</S.InfoTitle>
-              <S.InfoSpan>일반펌</S.InfoSpan>
-            </li>
-          </S.Info>
-        </S.ContentBox>
-      </S.DivideBox>
-      <S.ContentBox>
-        <h2>상세 희망 스타일</h2>
-        <S.InfoText />
-      </S.ContentBox>
+      </S.ContentSection>
       <Button
         text="지원하기"
         isFixed={true}
@@ -97,13 +101,35 @@ const S = {
     flex: 1;
     justify-content: center;
     align-items: center;
+    overflow: hidden;
 
     width: 100%;
     height: 100%;
     margin: 5.7rem 0 9.08rem;
-    padding: 0 2rem 2.3rem;
 
     ${({ theme }) => theme.fonts.Body01};
+  `,
+
+  ContentSection: styled.section`
+    display: flex;
+    flex-direction: column;
+    gap: 1.2rem;
+    overflow-y: scroll;
+
+    width: 100%;
+    margin-right: 8px;
+    padding: 0 2rem 2.3rem;
+
+    &::-webkit-scrollbar {
+      width: 4px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      border-radius: 12px;
+
+      background: ${({ theme }) => theme.colors.moddy_blue3};
+      background-clip: padding box;
+    }
   `,
 
   ContentBox: styled.div`
@@ -172,7 +198,8 @@ const S = {
     ${({ theme }) => theme.fonts.Body02};
   `,
 
-  InfoText: styled.textarea`
+  InfoText: styled.p`
+    padding: 1.2rem 1.55rem;
     border: none;
     border-radius: 10px;
     resize: none;
