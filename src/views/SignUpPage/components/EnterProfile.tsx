30452a5b397602c8845348af363a6e8e8a2b80df
Subin Hwang <46593078+binllionaire@users.noreply.github.com>
2024-01-08 20:19:13 +0900

    [SignUpPage] 회원가입 뷰 퍼블리싱 및 기능 구현 (#70)
    * feat: onClickFn props 추가
    
    * [Common] 공통 컴포넌트 - ProgressBar (#44)
    
    * feat: ProgressBar 기본 퍼블리싱
    
    * feat: whole, current에 따른 공통 컴포넌트화
    
    * [Common] 공통 컴포넌트 - Textarea, Input 구현  (#37)
    
    * design: textArea 기본 publishing
    
    * design: focus style 추가
    
    * feat: input 글자수 카운팅 및 제한
    
    * chore: 파일명 수정
    
    * chore: MainPage 복원
    
    * design: Input publishing
    
    * feat: input 텍스트 작성 후 focus out 시 체크
    
    * design: 0자일 때 gray font
    
    * chore: MainPage 복원
    
    * feat: placeholderText props 추가
    
    * feat: Input placeholderText props 추가
    
    * chore: 주석 삭제
    
    * add : 더미 이미지 추가
    
    * fix : 닫힘 태그 추가
    
    * design : 유저 유행 선택 퍼블리싱
    
    * design : 기본 정보 입력 퍼블리싱
    
    * add : 페이지 스텝 상수화
    
    * design : 개인 정보 입력 페이지 퍼블리싱
    
    * design : 회원가입 페이지 퍼블리싱
    
    * add : 필수 표시 아이콘 색상 변경
    
    * add : 아이콘 추가
    
    * chore : 텍스트 수정
    
    * design : 개인 정보 입력 퍼블리싱
    
    * design : border-radius 변경
    
    * design: ProgressBar position fixed
    
    * chore: moddy_blue5 추가
    
    * chore: IcStepOngoing 아이콘 수정
    
    * refactor : s-dot naming, field 이름 추상화
    
    * refactor : 컴포넌트 추상화
    
    * design: Header, Button fixed시 max-width 설정
    
    * [Common] CTA 추상화  (#48)
    
    * feat: disabled 여부 분기처리
    
    * feat: isFixed, disabled 속성 선택으로 변경
    
    * feat: icon 추가할 수 있도록 props
    
    * [Common] 공통 컴포넌트 - 모달 구현  (#50)
    
    * design: theme에 mooddy_bk20 추가
    
    * design: Modal 기본 publishing
    
    * feat: props 활용을 통한 추상화
    
    * [Common] 공통 컴포넌트 - 토스트 메시지 구현  (#52)
    
    * design: theme에 mooddy_bk20 추가
    
    * design: Modal 기본 publishing
    
    * feat: props 활용을 통한 추상화
    
    * design: ToastMessage 퍼블리싱
    
    * feat: props를 통한 토스트메시지 추상화
    
    * feat: 토스트메시지 타이머 설정
    
    * chore: gitkeep 삭제
    
    * refactor : s-dot네이밍으로 수정
    
    * refactor : s-dot 네이밍으로 수정
    
    * feature : useInterval 훅 추가
    
    * add : 유저 유형 상수 추가
    
    * add : 인증 관련 상수 추가
    
    * feature : 디자이너 플로우 조건 처리
    
    * design : 전화번호 인증 뷰 퍼블리싱
    
    * feature : 시술희망 지역 뷰 구현
    
    * fix : styled-component props 관련 경고 해결
    
    * fix : styled-component props 관련 경고 해결
    
    * add : 애니메이션 추가로 인한 사용단위 s 추가
    
    * refactor : 지역 선택 아이템 추상화
    
    * feature : 조건부 로직 추가
    
    * feature : 카테고리 영역 밖 클릭시 이벤트 처리
    
    * design : progress bar position 변경으로 인한 수정
    
    * remove : 미사용 import 구문 삭제
    
    * design : 간격 수정
    
    * fix : 바텀 시트 영역 클릭 시 예외 처리
    
    * design : 행간 수정
    
    * feature : 유저 유형에 따른 프로그레스 개수 처리
    
    * feature : 출생 연도 입력 검증 구현
    
    * fix : 입력 검증 하이픈 수정
    
    * fix : 출생 연도 입력 검증 수정
    
    * style : 개행 수정
    
    ---------
    
    Co-authored-by: lydiacho <sheecho0804@ewhain.net>
    Co-authored-by: lydiacho <81505421+lydiacho@users.noreply.github.com>

---
+++ src/views/SignUpPage/components/EnterProfile.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+import Header from '../../@common/components/Header';
+import { STEP } from '../constants/step';
+
+import PersonalInfo from './PersonalInfo';
+import SelectPrefeRegion from './SelectPreferRegion';
+import VerifyPhoneNumber from './VerifyPhoneNumber';
+
+const EnterProfile = () => {
+  const [step, setStep] = useState(STEP.PERSONAL_INFO);
+
+  const Contents = () => {
+    switch (step) {
+      case STEP.PERSONAL_INFO:
+        return <PersonalInfo setStep={setStep} />;
+      case STEP.PHONE_NUMBER_VERIFICATION:
+        return <VerifyPhoneNumber setStep={setStep} />;
+      case STEP.PREFER_REGION:
+        return <SelectPrefeRegion />;
+    }
+  };
+
+  const StepHeader = () => {
+    switch (step) {
+      case STEP.PERSONAL_INFO:
+        return <Header isBackBtnExist={true} isCloseBtnExist={false} title="프로필 작성" />;
+      case STEP.ADDRESS:
+        return <Header isBackBtnExist={true} isCloseBtnExist={true} title="주소 검색" />;
+      default:
+        return <Header isBackBtnExist={true} isCloseBtnExist={true} title="프로필 작성" />;
+    }
+  };
+
+  return (
+    <>
+      <StepHeader />
+      <Contents />
+    </>
+  );
+};
+export default EnterProfile;
---
+++ src/views/SignUpPage/components/Field.tsx
@@ -0,0 +1,41 @@
+import { styled } from 'styled-components';
+
+import { IcEssential } from '../../@common/assets/icons';
+interface FieldProp {
+  name: string;
+  isEssential: boolean;
+}
+
+const Field = (props: FieldProp) => {
+  const { name, isEssential } = props;
+
+  return (
+    <S.FieldBox>
+      <S.FieldSpan>{name}</S.FieldSpan>
+      {isEssential ? <IcEssential /> : null}
+    </S.FieldBox>
+  );
+};
+
+export default Field;
+
+const FieldBox = styled.div`
+  display: flex;
+
+  margin-top: 3.6rem;
+  margin-bottom: 1.1rem;
+
+  &:first-child {
+    margin-top: 0;
+  }
+`;
+
+const FieldSpan = styled.span`
+  color: ${({ theme }) => theme.colors.moddy_bk};
+  ${({ theme }) => theme.fonts.Headline01};
+`;
+
+const S = {
+  FieldBox,
+  FieldSpan,
+};
