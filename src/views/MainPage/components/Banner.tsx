e1f0e4a15cf0781b131c1d00b3d5fa54fadef755
binllionaire <bug20474@gmail.com>
2024-01-10 02:24:56 +0900

    feature : 배너 캐러셀 구현

--- src/views/MainPage/components/Banner.tsx
+++ src/views/MainPage/components/Banner.tsx
@@ -1,18 +1,79 @@
+import { EmblaCarouselType } from 'embla-carousel';
+import Autoplay from 'embla-carousel-autoplay';
+import useEmblaCarousel from 'embla-carousel-react';
+import { useCallback, useEffect, useState } from 'react';
 import { styled } from 'styled-components';
-
-import { USER_TYPE } from '../constants/constants';
 
 import banner1 from '@images/img_banner1.png';
 import banner2 from '@images/img_banner2.png';
+import '../styles/carousel.css';
 
-interface BannerProps {
-  userType: number;
-}
-const Banner = ({ userType }: BannerProps) => {
+const Banner = () => {
+  const slides = [banner1, banner2];
+
+  const [selectedindex, setSelectedIndex] = useState(0);
+  const [scrollSnaps, setScrollSnaps] = useState<number[]>([]);
+  const autoplayOptions = {
+    delay: 6000,
+    rootNode: (emblaRoot: HTMLElement) => emblaRoot.parentElement,
+  };
+
+  const [emblaRef, emblaApi] = useEmblaCarousel(
+    {
+      align: 'center',
+      loop: true,
+    },
+    [Autoplay(autoplayOptions)],
+  );
+
+  const scrollTo = useCallback((index: number) => emblaApi && emblaApi.scrollTo(index), [emblaApi]);
+
+  const onInit = useCallback((emblaApi: EmblaCarouselType) => {
+    setScrollSnaps(emblaApi.scrollSnapList());
+  }, []);
+
+  const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+    setSelectedIndex(emblaApi.selectedScrollSnap());
+  }, []);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    onInit(emblaApi);
+    onSelect(emblaApi);
+    emblaApi.on('reInit', onInit);
+    emblaApi.on('reInit', onSelect);
+    emblaApi.on('select', onSelect);
+  }, [emblaApi, onInit, onSelect]);
+
   return (
     <BannerLayout>
       <BannerBox>
-        <img src={userType === USER_TYPE.DESIGNER ? banner1 : banner2} alt="배너" />
+        <div className="embla">
+          <div className="embla__viewport" ref={emblaRef}>
+            <div className="embla__container">
+              {slides.map((banner, index) => (
+                <div className="embla__slide" key={index}>
+                  <div className="embla__slide__inner">
+                    <img src={banner} alt="배너" />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="embla__navigator">
+            {scrollSnaps.map((_, index) => (
+              <button
+                className="embla__dots"
+                key={index}
+                style={{
+                  backgroundColor: selectedindex === index ? '#A38EFF' : '#D3D1FF',
+                }}
+                onClick={() => scrollTo(index)}
+              />
+            ))}
+          </div>
+        </div>
       </BannerBox>
     </BannerLayout>
   );
@@ -25,11 +86,13 @@ const BannerLayout = styled.div`
 `;
 
 const BannerBox = styled.div`
+  position: relative;
+
   width: 100%;
   height: 8.4rem;
   border-radius: 8px;
 
-  & > img {
+  & img {
     width: 100%;
   }
 `;
