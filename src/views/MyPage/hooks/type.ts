c3989814c12893ded8ad6111199c9ef7b36a27fc
lydiacho <sheecho0804@ewhain.net>
2024-01-11 05:24:11 +0900

    feat: useGetUser 구현

---
+++ src/views/MyPage/hooks/type.ts
@@ -0,0 +1,12 @@
+export interface UserProps {
+  id: number;
+  profileImgUrl: string;
+  name: string;
+  role: string;
+}
+
+export interface UserResponse {
+  code: number;
+  message: string;
+  data: UserProps;
+}
