e54b822f4422583f5a68bc026ddcc8e9948f0057
lydiacho <sheecho0804@ewhain.net>
2024-01-13 03:21:34 +0900

    feat: AgreementList에서 상수 빼주기

--- src/views/AgreementPage/components/AgreementList.tsx
+++ src/views/AgreementPage/components/AgreementList.tsx
@@ -1,6 +1,8 @@
 import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
 
+import { AGREEMENT_LINK, AGREEMENT_TOTAL_COUNT } from '../constants/AGREEMENT_LINK';
+
 import AgreementItem from './AgreementItem';
 
 import { agreementState } from '@/recoil/atoms/agreementState';
@@ -12,7 +14,7 @@ const AgreementList = () => {
 
   const handleCheck = (idx: number) => {
     if (idx === 0) {
-      setChecked(new Array(4).fill(!isChecked[0]));
+      setChecked(new Array(AGREEMENT_TOTAL_COUNT).fill(!isChecked[0]));
       isChecked[3] ? setMarketingAgree(false) : setMarketingAgree(true);
     } else {
       if (idx === 3) {
@@ -38,19 +40,19 @@ const AgreementList = () => {
         text="모디 서비스 이용약관 동의"
         isChecked={isChecked[1]}
         onClickCheck={() => handleCheck(1)}
-        link="https://moddy.notion.site/b6d316bce4f0476f812c2bff25ec6bf8?pvs=4"
+        link={AGREEMENT_LINK.useService}
       />
       <AgreementItem
         text="개인정보 수집 및 이용 동의"
         isChecked={isChecked[2]}
         onClickCheck={() => handleCheck(2)}
-        link="https://moddy.notion.site/b5837d44058647eabc646492aa4a12a2?pvs=4"
+        link={AGREEMENT_LINK.personalInfo}
       />
       <AgreementItem
         text="마케팅 정보 수신 동의 (선택)"
         isChecked={isChecked[3]}
         onClickCheck={() => handleCheck(3)}
-        link="https://moddy.notion.site/7afa88127b754445b8eba475414b587a?pvs=4"
+        link={AGREEMENT_LINK.marketing}
       />
     </S.AgreementListLayout>
   );
