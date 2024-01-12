29ae344b98d36e866ed767a1b98e1cf0fc4ff4d0
lydiacho <sheecho0804@ewhain.net>
2024-01-12 23:06:33 +0900

    feat: kakaoCodeState recoil persist atom 생성

--- src/views/LoginPage/hooks/usePostLogin.ts
+++ src/views/LoginPage/hooks/usePostLogin.ts
@@ -1,13 +1,16 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
 
 import { loginErrorProps, loginResProps } from './type';
 
+import { kakaoCodeState } from '@/recoil/atoms/kakaoCodeState';
 import api from '@/views/@common/hooks/api';
 
 const usePostLogin = () => {
   const KAKAO_CODE = new URL(window.location.href).searchParams.get('code');
   const navigate = useNavigate();
+  const setKakaoCode = useSetRecoilState(kakaoCodeState);
 
   useEffect(() => {
     api
@@ -23,6 +26,7 @@ const usePostLogin = () => {
       })
       .catch((err: loginErrorProps) => {
         if (err.response.data.code === 404) {
+          setKakaoCode(KAKAO_CODE);
           navigate('/agreement');
         } else {
           navigate('/error');
