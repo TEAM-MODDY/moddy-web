ecce9ffbc141d7188879f27b7c6173285ad1c975
Minseo Kang <rachel5640@naver.com>
2024-01-13 19:29:17 +0900

    fix: 코드리뷰 반영

--- src/views/OfferInfoPage/hooks/useGetOfferModel.ts
+++ src/views/OfferInfoPage/hooks/useGetOfferModel.ts
@@ -13,21 +13,21 @@ const useGetModel = () => {
   const [error, setError] = useState<AxiosError>();
 
   const fetchData = async () => {
-    await OfferInfoApi.get('/model/offer/1', {
-      headers: {
-        Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJBQ0NFU1NfVE9LRU4iLCJpYXQiOjE3MDQ5OTg1OTMsImV4cCI6MTcwNzU5MDU5MywiVVNFUl9JRCI6IjgifQ.YQO9cpo2qJviduAynEBO6kQVMWLjIehU_OnIGYqGSbpi4UkcbahfwSwKvjdEO-bs`,
-      },
-    })
-      .then((res) => {
-        setData(res.data.data);
-      })
-      .catch((err) => {
+    try {
+      const response = await OfferInfoApi.get('/model/offer/1', {
+        headers: {
+          Authorization: `Bearer {code}`,
+        },
+      });
+      setData(response.data.data);
+    } catch (err) {
+      if (err instanceof AxiosError) {
         setError(err);
         navigate('/error');
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+      }
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
