d7c82c9271bdf8cccec4d172bcb5f559410f0fb2
lydiacho <sheecho0804@ewhain.net>
2024-01-15 15:58:59 +0900

    chore: removeAccessToken -> removeToken

--- src/views/@common/hooks/api.ts
+++ src/views/@common/hooks/api.ts
@@ -14,7 +14,7 @@ export const setToken = (token: string) => {
   localStorage.setItem(ACCESS_TOKEN, token);
 };
 
-export const removeAccessToken = () => {
+export const removeToken = () => {
   localStorage.removeItem(ACCESS_TOKEN);
 };
 
