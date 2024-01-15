5553c563fdde48953e9e7926ad343f7a3082e948
Subin Hwang <46593078+binllionaire@users.noreply.github.com>
2024-01-15 23:33:54 +0900

    [MainPage] 디자이너 메인 뷰 API (#208)
    * add : react-intersection-observer 라이브러리 추가
    
    * feature : 무한스크롤 페이지네이션 구현
    
    * style : 속성 순서 수정
    
    * rename : 컴포넌트 명 변경
    
    * add : gurest 상수 추가
    
    * add : 선호 헤어 스타일 상수 추가
    
    * style : userType 전역 상태관리 안쓰고 props로 받도록 수정
    
    * add : 디자이너 리스폰스 타입 추가
    
    * feature : 디자이너 메인 뷰 API 추가
    
    * feature : 디자이너/모델 메인 뷰 조건부 렌더링
    
    * fix : 타입 관련 에러 수정
    
    * feature : 전체 데이터 개수로 요청 제한 및 타입 에러 해결
    
    * add : 이름 필드 값 통일 및 total 타입 추가
    
    * style : 전역 상태 사용 -> props 사용
    
    * update : 서버 ENUM 변경으로 인한 수정
    
    * fix : guest일 떄 TopSheet 안나오는 이슈 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,30 +1,48 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { styled } from 'styled-components';
 
 import Banner from '../views/MainPage/components/Banner';
-import Contents from '../views/MainPage/components/Contents';
-import ReceivedOffer from '../views/MainPage/components/ReceivedOffer';
+import GuestContents from '../views/MainPage/components/GuestContents';
 import StatusBarForiOS from '../views/MainPage/components/StatusBarForiOS';
 import TopSheet from '../views/MainPage/components/TopSheet';
+import { ReceivedOffer, ReceivedApplication } from '../views/MainPage/components/UserContents';
 
 import { userTypeState } from '@/recoil/atoms/signUpState';
-import useGetModel from '@/views/MainPage/hooks/useGetModel';
+import { USER_TYPE } from '@/views/@common/constants/userType';
+import useGetMain from '@/views/MainPage/hooks/useGetMain';
 
 const MainPage = () => {
   const userType = useRecoilValue(userTypeState);
-  // const userType = 'model';
+  // const userType = USER_TYPE.GUEST;
 
   const [page, setPage] = useState(1);
-  const { data } = useGetModel({ page: page });
+  const { data } = useGetMain({ user: userType, page: page });
+
+  const MainContents = () => {
+    switch (userType) {
+      case USER_TYPE.GUEST:
+        return <GuestContents />;
+      case USER_TYPE.DESIGNER:
+        return data && <ReceivedApplication data={data} setPage={setPage} />;
+      case USER_TYPE.MODEL:
+        return data && <ReceivedOffer data={data} setPage={setPage} />;
+      default:
+        return null;
+    }
+  };
 
   return (
     <>
       <MainPageLayout>
         <StatusBarForiOS />
-        <TopSheet applyType={data?.status} name={data?.userName} />
+        <TopSheet
+          userType={userType}
+          applyType={userType === USER_TYPE.MODEL && data && 'status' in data ? data.status : ''}
+          name={data ? data.name : ''}
+        />
         <Banner />
-        {!userType ? <Contents /> : data && <ReceivedOffer data={data} setPage={setPage} />}
+        <MainContents />
       </MainPageLayout>
     </>
   );
