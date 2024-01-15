d870b0c5eaa383df969fe8d7b98cbe9ff58b4fef
lydiacho <sheecho0804@ewhain.net>
2024-01-16 00:33:51 +0900

    feat: Login 로직 수정사항 반영

--- src/views/LoginPage/hooks/usePostLogin.ts
+++ src/views/LoginPage/hooks/usePostLogin.ts
@@ -1,16 +1,13 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
 
 import { loginErrorProps, loginResProps } from './type';
 
-import { kakaoCodeState } from '@/recoil/atoms/kakaoCodeState';
-import api from '@/views/@common/hooks/api';
+import api, { setToken } from '@/views/@common/hooks/api';
 
 const usePostLogin = () => {
   const KAKAO_CODE = new URL(window.location.href).searchParams.get('code');
   const navigate = useNavigate();
-  const setKakaoCode = useSetRecoilState(kakaoCodeState);
 
   useEffect(() => {
     api
@@ -21,13 +18,15 @@ const usePostLogin = () => {
       })
       .then((res: loginResProps) => {
         console.log('로그인 성공');
-        console.log(res);
+        console.log(res.data.data.accessToken);
         // 로그인완료되고 메인뷰로 이동
+        setToken(res.data.data.accessToken);
+        navigate('/');
       })
       .catch((err: loginErrorProps) => {
         if (err.response.data.code === 404) {
-          setKakaoCode(KAKAO_CODE);
           navigate('/agreement');
+          setToken(err.response.data.data.accessToken);
         } else {
           navigate('/error');
         }
