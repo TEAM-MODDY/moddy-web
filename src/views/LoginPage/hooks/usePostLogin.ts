76e6d624d0daf0b747a48d82e31666bc85237df3
lydiacho <sheecho0804@ewhain.net>
2024-01-16 00:38:53 +0900

    feat: userType에 role 저장

--- src/views/LoginPage/hooks/usePostLogin.ts
+++ src/views/LoginPage/hooks/usePostLogin.ts
@@ -1,13 +1,16 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
 
 import { loginErrorProps, loginResProps } from './type';
 
+import { userTypeState } from '@/recoil/atoms/signUpState';
 import api, { setToken } from '@/views/@common/hooks/api';
 
 const usePostLogin = () => {
   const KAKAO_CODE = new URL(window.location.href).searchParams.get('code');
   const navigate = useNavigate();
+  const setUserType = useSetRecoilState(userTypeState);
 
   useEffect(() => {
     api
@@ -21,12 +24,13 @@ const usePostLogin = () => {
         console.log(res.data.data.accessToken);
         // 로그인완료되고 메인뷰로 이동
         setToken(res.data.data.accessToken);
+        setUserType(res.data.data.role);
         navigate('/');
       })
       .catch((err: loginErrorProps) => {
         if (err.response.data.code === 404) {
-          navigate('/agreement');
           setToken(err.response.data.data.accessToken);
+          navigate('/agreement');
         } else {
           navigate('/error');
         }
