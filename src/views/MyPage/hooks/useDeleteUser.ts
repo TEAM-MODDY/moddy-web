0642c36597b90c87973ebf0f1667d7459d3aa960
lydiacho <sheecho0804@ewhain.net>
2024-01-15 15:57:43 +0900

    feat: useDeleteUser 구현

--- src/views/MyPage/hooks/useDeleteUser.ts
+++ src/views/MyPage/hooks/useDeleteUser.ts
@@ -1,28 +1,25 @@
-import { AxiosError } from 'axios';
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import api from '@/views/@common/hooks/api';
 
 const useDeleteUser = () => {
   const navigate = useNavigate();
-  const [isLoading, setLoading] = useState(false);
-  const [isError, setError] = useState<AxiosError>();
 
   const deleteUser = async () => {
     try {
-      await api.delete('/user');
+      await api.delete('/user', {
+        headers: {
+          Authorization:
+            'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJBQ0NFU1NfVE9LRU4iLCJpYXQiOjE3MDQ5OTgyOTYsImV4cCI6MTcwNzU5MDI5NiwiVVNFUl9JRCI6IjYifQ.WPDNRjbbAnKbOwSr2T3XkdmvzWSpIJrh3Plluh2c-ak3LTYSLnpOUxCgk2a3xmQg',
+        },
+      });
       navigate('/');
     } catch (err) {
-      if (err instanceof AxiosError) setError(err);
       navigate('/error');
     }
-    setLoading(false);
   };
 
-  deleteUser();
-
-  return { isLoading, isError };
+  return deleteUser;
 };
 
 export default useDeleteUser;
