bba78c0c59f54ba62946d2b25b3b63f416e8a6a2
Minseo Kang <rachel5640@naver.com>
2024-01-16 22:50:15 +0900

    feat: AccessToken 삭제

--- src/views/ModelInfoPage/hooks/useGetApplication.ts
+++ src/views/ModelInfoPage/hooks/useGetApplication.ts
@@ -16,8 +16,7 @@ const useGetApplication = (applicationId: number) => {
     try {
       const response = await api.get(`/designer/${applicationId}`, {
         headers: {
-          Authorization:
-            'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJBQ0NFU1NfVE9LRU4iLCJpYXQiOjE3MDQ5OTg1OTMsImV4cCI6MTcwNzU5MDU5MywiVVNFUl9JRCI6IjgifQ.YQO9cpo2qJviduAynEBO6kQVMWLjIehU_OnIGYqGSbpi4UkcbahfwSwKvjdEO-bs',
+          Authorization: 'Bearer ~',
         },
       });
       setData(response.data.data);
