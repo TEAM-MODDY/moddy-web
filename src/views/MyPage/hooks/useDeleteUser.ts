e559b1940bacb6035dc56e112990ef592d71a46f
lydiacho <sheecho0804@ewhain.net>
2024-01-17 19:38:55 +0900

    fix: useResetRecoilState 활용하여 userType 초기화

--- src/views/MyPage/hooks/useDeleteUser.ts
+++ src/views/MyPage/hooks/useDeleteUser.ts
@@ -1,20 +1,19 @@
 import { useNavigate } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
+import { useResetRecoilState } from 'recoil';
 
 import { userTypeState } from '@/recoil/atoms/signUpState';
-import { USER_TYPE } from '@/views/@common/constants/userType';
 import api from '@/views/@common/hooks/api';
 import removeToken from '@/views/@common/utils/removeToken';
 
 const useDeleteUser = () => {
   const navigate = useNavigate();
-  const setUserType = useSetRecoilState(userTypeState);
+  const resetType = useResetRecoilState(userTypeState);
 
   const deleteUser = async () => {
     try {
       await api.delete('/user');
       removeToken();
-      setUserType(USER_TYPE.GUEST);
+      resetType();
       navigate('/');
     } catch (err) {
       navigate('/error');
--- src/views/MyPage/hooks/usePostLogout.ts
+++ src/views/MyPage/hooks/usePostLogout.ts
@@ -1,20 +1,18 @@
 import { useNavigate } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
+import { useResetRecoilState } from 'recoil';
 
 import { userTypeState } from '@/recoil/atoms/signUpState';
-import { USER_TYPE } from '@/views/@common/constants/userType';
 import api from '@/views/@common/hooks/api';
 import removeToken from '@/views/@common/utils/removeToken';
 
 const usePostLogout = () => {
   const navigate = useNavigate();
-  const setUserType = useSetRecoilState(userTypeState);
-
+  const resetType = useResetRecoilState(userTypeState);
   const postLogout = async () => {
     try {
       await api.post('/auth/logout', null);
       removeToken();
-      setUserType(USER_TYPE.GUEST);
+      resetType();
       navigate('/');
     } catch (err) {
       console.log(err);
