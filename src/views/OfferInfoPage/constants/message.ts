4182afa617bb185cf71d1ba12a709386e4d79e85
Minseo Kang <rachel5640@naver.com>
2024-02-11 22:28:46 +0900

    fix: Pages안에 들어있던 컴포넌트 위치 변경

--- src/views/OfferInfoPage/constants/message.ts
+++ src/views/OfferInfoPage/constants/message.ts
@@ -6,3 +6,12 @@ export const OFFER_INFO_BTN = {
   INSTAGRAM: '인스타그램',
   NAVER_PLACE: '네이버 플레이스',
 };
+
+export const OFFER_INFO_CATEGORY = {
+  PREFER_STYLE: '요청 스타일',
+  DESIGNER_OFFER: '디자이너 상세 제안',
+  APPLICATION_DETAIL: '상세 희망 스타일',
+  DESIGNER_INFO: '디자이너 정보',
+  GENDER: '성별',
+  DAYOFF: '휴무일',
+};
