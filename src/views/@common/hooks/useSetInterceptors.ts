37080ac5f17935e329df19408203a8491da2e057
lydiacho <sheecho0804@ewhain.net>
2024-02-13 00:15:59 +0900

    feat: useSetInterceptors 리프레시 만료 예외 처리

--- src/views/@common/hooks/useSetInterceptors.ts
+++ src/views/@common/hooks/useSetInterceptors.ts
@@ -19,7 +19,7 @@ const useSetInterceptors = () => {
     api.interceptors.response.use(
       (res) => res,
       async (err) => {
-        if (err.response.status === 401) {
+        if (err.config.url !== '/auth/refresh' && err.response.status === 401) {
           await postRefresh();
           const token = getToken();
           err.config.headers['Authorization'] = `Bearer ${token}`;
