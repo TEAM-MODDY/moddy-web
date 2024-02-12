fe194d11cea08bb87ff4e6023dbfe364edea6c91
lydiacho <sheecho0804@ewhain.net>
2024-02-13 00:27:05 +0900

    remove: 불필요한 resetType 삭제'

--- src/views/MyPage/hooks/usePostLogout.ts
+++ src/views/MyPage/hooks/usePostLogout.ts
@@ -1,18 +1,14 @@
 import { useNavigate } from 'react-router-dom';
-import { useResetRecoilState } from 'recoil';
 
-import { userTypeState } from '@/recoil/atoms/signUpState';
 import api from '@/views/@common/hooks/api';
 import { removeToken } from '@/views/@common/utils/token';
 
 const usePostLogout = () => {
   const navigate = useNavigate();
-  const resetType = useResetRecoilState(userTypeState);
   const postLogout = async () => {
     try {
       await api.post('/auth/logout', null);
       removeToken();
-      resetType();
       navigate('/');
     } catch (err) {
       console.log(err);
