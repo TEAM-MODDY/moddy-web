b928b82ec68d238a29e4672dd8eacdde5696f584
binllionaire <bug20474@gmail.com>
2024-02-15 21:17:10 +0900

    add : 상수 추가

---
+++ src/views/SignUpPage/constants/constants.ts
@@ -0,0 +1,10 @@
+export const BIRTH_YEAR_LENGTH = 4;
+export const NAME_MAX_LENGTH = 5;
+export const SECONDS_PER_MINUTE = 60;
+export const PHONE_NUMBER_FORMAT_TEMPLATE = '$1-$2-$3';
+export const LIMIT_TIME = 180;
+export const HYPHEN = '-';
+export const EMPTY_STRING = '';
+export const PREFER_REGION_MAX_COUNT = 3;
+export const PREFER_REGION_MIN_COUNT = 1;
+export const SELECT_ALL = 0;
