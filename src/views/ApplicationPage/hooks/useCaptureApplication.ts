f87fb0b0e7caf4e2da7a0eaa3ba25aa7ae442450
aazkgh <gg25hp17@naver.com>
2024-02-25 05:15:36 +0900

    fix: GET 무한 렌더링 이슈 해결

--- src/views/ApplicationPage/hooks/useCaptureApplication.ts
+++ src/views/ApplicationPage/hooks/useCaptureApplication.ts
@@ -4,23 +4,33 @@ import { useSetRecoilState } from 'recoil';
 
 import { applicationCaptureImgUrlState } from '@/recoil/atoms/applicationState';
 
-export const useCaptureApplication = async (ref: HTMLElement | null) => {
+export const useCaptureApplication = (ref: HTMLElement | null) => {
   const [image, setImage] = useState<HTMLElement>();
   const setImgData = useSetRecoilState(applicationCaptureImgUrlState);
 
   useEffect(() => {
-    ref && setImage(ref);
+    if (ref && !image) {
+      setImage(ref);
+    }
   }, [ref]);
 
-  if (image) {
-    const canvas = await html2canvas(image);
-    if (canvas.height !== 0) {
-      const imgUrl = canvas.toDataURL();
-      const response = await fetch(imgUrl);
-      const blob = await response.blob();
-      const file = new File([blob], 'captureApplication.png', { type: 'image/png' });
+  useEffect(() => {
+    const captureImage = async () => {
+      if (image) {
+        const canvas = await html2canvas(image);
+        if (canvas.height !== 0) {
+          const imgUrl = canvas.toDataURL();
+          const response = await fetch(imgUrl);
+          const blob = await response.blob();
+          const file = new File([blob], 'captureApplication.png', { type: 'image/png' });
 
-      setImgData({ applicationCaptureImgUrl: file });
+          setImgData({ applicationCaptureImgUrl: file });
+        }
+      }
+    };
+
+    if (image) {
+      captureImage();
     }
-  }
+  }, [image]);
 };
