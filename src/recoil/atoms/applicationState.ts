831e27a978611d2ed5f64359122a45edfe8daee1
고가형 <101045330+aazkgh@users.noreply.github.com>
2024-03-11 14:23:11 +0100

    [Merge] feature/SP2 => develop (#425)
    * [ApplicationPage] 시술 내역 드롭다운 박스 단일 활성화 기능 추가 (#385)
    
    refactor: 불필요한 코드 삭제
    feat: ref 훅 추가
    refactor: 시술 선택, 기간 선택 state 통합
    fix: state 조건 추가
    feat: 단일 드롭다운 활성화
    feat: 외부 영역 클릭시 드롭다운 선택 해제
    chore: 불필요한 코드 제거
    refactor: 중복되는 코드 정리
    
    * [ApplicationmPage] 시술 내역 경고 모달창 추가 (#422)
    
    feat: 경고 모달창 구현
    refactor: 드롭다운 박스 선택 범위 수정

--- src/recoil/atoms/applicationState.ts
+++ src/recoil/atoms/applicationState.ts
@@ -43,7 +43,7 @@ export interface historyDetailProps {
 }
 
 export interface historyType {
-  hairServiceRecords: Array<historyDetailProps>;
+  hairServiceRecords: historyDetailProps[];
   hairService: string;
   hairServiceTerm: string;
 }
