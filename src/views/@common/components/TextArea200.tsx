ae08214c1221b0af2ce35d3e38313a2cf29156bf
Minseo Kang <rachel5640@naver.com>
2024-04-28 01:45:44 +0900

    remove: 불필요한 props 삭제

--- src/views/@common/components/TextArea200.tsx
+++ src/views/@common/components/TextArea200.tsx
@@ -8,7 +8,7 @@ interface TextArea200Props {
   id?: string;
 }
 
-const TextArea200 = ({ placeholderText, initialValue, onChangeFn, id }: TextArea200Props) => {
+const TextArea200 = ({ placeholderText, initialValue, onChangeFn }: TextArea200Props) => {
   const [textLength, setTextLength] = useState(initialValue ? initialValue.length : 0);
   const [name, setName] = useState(initialValue ? initialValue : '');
 
@@ -23,7 +23,6 @@ const TextArea200 = ({ placeholderText, initialValue, onChangeFn, id }: TextArea
           onChangeFn(e.target.value);
         }}
         maxLength={200}
-        id={id}
       />
       <S.TextAreaSpan>
         <S.TextAreaCountSpan $isZero={textLength === 0}>{textLength}</S.TextAreaCountSpan> / 200
