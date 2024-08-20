4f7b83d90a5dbb3b9546fd02d5be01d53de7be8e
lydiacho <sheecho0804@ewhain.net>
2024-08-20 20:17:29 +0900

    remove: 불필요한 파일 삭제

--- src/views/LoginPage/assets/index.ts
+++ src/views/LoginPage/assets/index.ts
@@ -1,19 +1,8 @@
 import IcCheck from './icons/ic_check.svg';
-import IcStep from './icons/ic_progress.svg';
-import IcStepOngoing from './icons/ic_step_ongoing.svg';
 import ImgBackground from './images/img_Background.png';
 import ImgKakaotalk from './images/img_kakaotalk.svg';
 import ImgOnboarding1 from './images/onboarding1.png';
 import ImgOnboarding2 from './images/onboarding2.png';
 import ImgOnboardingLogo from './images/onboardingLogo.png';
 
-export {
-  ImgKakaotalk,
-  IcCheck,
-  ImgOnboarding1,
-  ImgOnboarding2,
-  ImgOnboardingLogo,
-  ImgBackground,
-  IcStep,
-  IcStepOngoing,
-};
+export { ImgKakaotalk, IcCheck, ImgOnboarding1, ImgOnboarding2, ImgOnboardingLogo, ImgBackground };
