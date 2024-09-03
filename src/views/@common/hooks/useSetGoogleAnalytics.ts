85c776c02166d50263e3478d1712434f465ce434
lydiacho <sheecho0804@ewhain.net>
2024-09-03 21:37:18 +0900

    feat: GA initialize

--- src/views/@common/hooks/useSetGoogleAnalytics.ts
+++ src/views/@common/hooks/useSetGoogleAnalytics.ts
@@ -1,22 +1,17 @@
-import { createBrowserHistory } from 'history';
 import { useEffect } from 'react';
-import { initialize, set as setGA, pageview } from 'react-ga';
+import ReactGA from 'react-ga4';
+import { useLocation } from 'react-router-dom';
 
 const useSetGoogleAnalytics = () => {
-  const setGoogleAnalytics = () => {
-    // google analytics 관련
-    const gaTrackingID = import.meta.env.VITE_GA_TRACKING_ID;
-    initialize(gaTrackingID, { debug: true });
-    const history = createBrowserHistory();
-    history.listen((response) => {
-      setGA({ page: response.location.pathname });
-      pageview(response.location.pathname);
-    });
-  };
+  useEffect(() => {
+    ReactGA.initialize(import.meta.env.VITE_GA_TRACKING_ID);
+  }, []);
+
+  const location = useLocation();
 
   useEffect(() => {
-    setGoogleAnalytics();
-  }, []);
+    ReactGA.send({ hitType: 'pageview', page: location.pathname });
+  }, [location.pathname]);
 };
 
 export default useSetGoogleAnalytics;
