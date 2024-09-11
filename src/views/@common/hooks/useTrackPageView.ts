5d217efbeefed0b0a6e2118f898db10f9db48a69
lydiacho <sheecho0804@ewhain.net>
2024-09-11 15:23:50 +0900

    fix: page view 이벤트 트래커 수정

--- src/views/@common/hooks/useTrackPageView.ts
+++ src/views/@common/hooks/useTrackPageView.ts
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 const useTrackPageView = (step: number) => {
   const location = useLocation();
   if (typeof window.gtag === 'function') {
-    window.gtag('config', import.meta.env.VITE_GA_TRACKING_ID, {
+    window.gtag('event', 'page_view', {
       page_path: `${location.pathname}/step${step}`,
     });
   }
